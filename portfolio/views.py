99f7e29234147d1c4977508d3c0b46ea5b9244e1
TechCat <a.m.techcat@gmail.com>
2025-06-25 06:55:38 +0900

    Initial commit

---
+++ portfolio/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Project
+from django.core.mail import send_mail  # 開発時は print でもOK
+from .forms import ContactForm
+
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'home.html', {'projects': projects})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # send_mail(...) ここは開発中なら print で代用
+            print(form.cleaned_data)
+            return render(request, 'contact_success.html')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
