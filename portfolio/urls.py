f19b416455e05830c50c16b79abe027f95262427
TechCat <a.m.techcat@gmail.com>
2025-06-28 19:28:06 +0900

    Change navbar title to 'Home'

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'portfolio'
 
 urlpatterns = [
-    path('', views.project_list, name='home'),  # トップページ
+    path('', views.home_view, name='home'),  # トップページ
     path('apps/', views.project_list, name='apps_list'),  # /apps/ に一覧ページ
     path('inventory-app/', views.inventory_detail_view, name='inventory_detail'),  # 特定のアプリ詳細
-    path('<slug:slug>/', views.project_detail, name='project_detail'),  # slugによる詳細ページ
     path('contact/', views.contact, name='contact'),
+    path('<slug:slug>/', views.project_detail, name='project_detail'),  # slugによる詳細ページ
     path('quake_viz/', views.quake_detail_view, name='quake_detail'),
     path('co2_viz/', views.co2_detail_view, name='co2_detail'),
 ]
