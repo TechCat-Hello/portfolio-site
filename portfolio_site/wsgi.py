d187623b3b1f51b20b46e8cdd447725d714e686a
TechCat <a.m.techcat@gmail.com>
2025-06-30 21:52:33 +0900

    Fix NameError: define BASE_DIR in wsgi.py

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -10,9 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
-application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')
+application.add_files(str(BASE_DIR / 'media'), prefix='media/')
