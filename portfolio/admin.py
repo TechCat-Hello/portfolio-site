7c46d0a2885033794a09257542e14cbefdce4ed8
TechCat <a.m.techcat@gmail.com>
2025-07-02 14:09:24 +0900

    Display line breaks in project description in admin panel and Replace user_dashboard screenshot

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Project
+from django.utils.safestring import mark_safe
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'order', 'created_at')  # 一覧画面で表示するフィールド
     ordering = ('order',)  # 管理画面でもorder順で表示
+
+    def formatted_description(self, obj):
+        # 改行を <br> に変換して mark_safe で HTML として表示
+        return mark_safe(obj.description.replace('\n', '<br><br>'))
+
+    formatted_description.short_description = 'Description'
 
 admin.site.register(Project, ProjectAdmin)
 
